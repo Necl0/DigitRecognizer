45cb5fefca00d6c9ab1db0e019ab2aa8787a6966
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2022-11-19 23:25:02 -0500

    Update ethanstreamlit.py

--- ethanstreamlit.py
+++ ethanstreamlit.py
@@ -3,6 +3,7 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
 import pickle
+import cv2
 
 # title of the app
 st.title("Digit Recognizer")
@@ -18,31 +19,25 @@ canvas_result = st_canvas(
     update_streamlit=realtime_update,
     height=100,
     width=100,
+    background_color= "White",
     drawing_mode=drawing_mode,
     key="canvas",
     stroke_color="rgba(0, 0, 0, 1)",
 )
 
-def predict_img(img):
+if st.button("Predict"):
     model = pickle.load(open('model.pkl', 'rb'))
 
-    # predict digit
-    prediction = model.predict(img)
-    return prediction
+    # convert canvas content to png
+    img = Image.fromarray(np.uint8(canvas_result.image_data))
 
-# add button that when clicked calls predict_img on numpy array version of canvas content
-if st.button("Predict"):
-    img = Image.fromarray(canvas_result.image_data.astype('uint8'), 'RGB')
-    img = img.convert('L')
-    # preprocess image
-    img = img.resize((28, 28))
-    img = np.array(img)
-    img = img.reshape(1, 28, 28)
-    img = img.astype('float32')
-    img /= 255
-    img = np.set_printoptions(threshold=5)
-    print(img)
-    # predict digit
-    prediction = predict_img(img)
+
+    img.save('temp.png')
+
+    # convert image to numpy array
+    img = 1 - (np.asarray(Image.open("./temp.png").convert("L").resize((28, 28))) / 255)
+
+    prediction = model.predict(img[None, :, :]).argmax()
     print(prediction)
-    st.write("The digit is: ", prediction.argmax())
+    # display result
+    st.write("Prediction: ", prediction)
