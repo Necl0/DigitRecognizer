001ebfd3754c60e21ecc0f4e4713606211140d42
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2022-11-20 02:01:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,74 +3,61 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
 import pickle
-import random
+from random import randint
 
 # title of the app
 st.title("Digit Recognizer")
 
-def gen_problem():
-    kind = random.choice(['add','sub','mul','div'])
-    while True:
-        a,b = [random.randrange(1,99) for _ in range(2)]
-        match kind:
-            case 'add':
-                if a+b in range(1, 10):
-                    return (f"{a} + {b}"), (f"{a+b}")
-            case 'sub':
-                if a-b in range(1, 10):
-                    return (f"{a} - {b}"), (f"{a-b}")
-            case 'div':
-                if a//b in range(1, 10):
-                    return (f"{a} / {b}"), (f"{a//b}")
-            case 'mul':
-                if a*b in range(1, 10):
-                    return (f"{a} * {b}"), (f"{a*b}")
-def printEquations():
-    st.write("Equation: ")
+def getop(op):
+    ans = -1
+    while ans not in range(10):
+        a = randint(-99,99)
+        b = randint(-99,99)
+        if op == '+': ans = a+b
+        if op == '*': ans = a*b
+    return f'{a:3} {op} {b:3}'
 
-    eq, ans = gen_problem()
+st.write("Equation: ")
+eq = getop('+')
 
-    st.write(eq)
-    is_done = True
+st.write(eq)
+is_done = True
 
 # Specify canvas parameters in application
-while is_done == True:
-    drawing_mode = "freedraw"
-    stroke_width = 3
-    realtime_update = True
+drawing_mode = "freedraw"
+stroke_width = 3
+realtime_update = True
 
     # Create a canvas component
-    canvas_result = st_canvas(
-        stroke_width=stroke_width,
-        update_streamlit=realtime_update,
-        height=100,
-        width=100,
-        background_color= "White",
-        drawing_mode=drawing_mode,
-        key="canvas",
-        stroke_color="rgba(0, 0, 0, 1)",
-    )
+canvas_result = st_canvas(
+    stroke_width=stroke_width,
+    update_streamlit=realtime_update,
+    height=100,
+    width=100,
+    background_color= "White",
+    drawing_mode=drawing_mode,
+    key="canvas",
+    stroke_color="rgba(0, 0, 0, 1)",
+)
 
-    if canvas_result.image_data is not None:
-        if st.button("Predict"):
-            model = pickle.load(open('model.pkl', 'rb'))
+if st.button("Predict"):
+    model = pickle.load(open('model.pkl', 'rb'))
 
             # convert canvas content to png
-            img = Image.fromarray(np.uint8(canvas_result.image_data))
+    img = Image.fromarray(np.uint8(canvas_result.image_data))
 
 
-            img.save('temp.png')
+    img.save('temp.png')
 
-            # convert image to numpy array
-            img = 1 - (np.asarray(Image.open("./temp.png").convert("L").resize((28, 28))) / 255)
+        # convert image to numpy array
+    img = 1 - (np.asarray(Image.open("./temp.png").convert("L").resize((28, 28))) / 255)
 
-            prediction = model.predict(img[None, :, :]).argmax()
-            print(prediction)
+    prediction = model.predict(img[None, :, :]).argmax()
+    print(prediction)
 
             # display result
-            if prediction == ans:
-                st.write("Correct!")
-            else:
-                st.write("Incorrect!")
-            
-            st.stop()
+    # if prediction == ans:
+    #     st.write("Correct!")
+    # else:
+    #     st.write("Incorrect!")
+        
