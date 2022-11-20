2a114b9747f233889d99bba9b2cf1f79d26a08e8
Josiah <63004866+Necl0@users.noreply.github.com>
2022-11-19 20:30:05 -0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -14,13 +14,13 @@ realtime_update = True
 
 # Create a canvas component
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     update_streamlit=realtime_update,
     height=100,
     width=100,
     drawing_mode=drawing_mode,
     key="canvas",
+    stroke_color="rgba(0, 0, 0, 1)",
 )
 
 def predict_img(img):
@@ -35,7 +35,13 @@ if st.button("Predict"):
     img = Image.fromarray(canvas_result.image_data.astype('uint8'), 'RGB')
     img = img.convert('L')
     # preprocess image
-    img = np.array(img.resize((28, 28))).reshape(1, 784)
-    prediction = predict_img(img)[0]
-    st.write(f"Predicted Digit: {prediction}")
+    img = img.resize((28, 28))
+    img = np.array(img)
+    img = img.reshape(1, 28, 28, 1)
+    img = img.astype('float32')
+    img /= 255
+    # predict digit
+    prediction = predict_img(img)
+    print(prediction)
+    st.write("The digit is: ", np.argmax(prediction))
 
